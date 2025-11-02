a5717288529e2edd4dc83c89be2d18dd03e740ef
Shashank777 <shashank@vistaintelligence.ai>
2025-11-02 16:07:43 +0530

    Re Superuser create

--- create_admin.py
+++ create_admin.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-username = "admin"
+username = "testuser"
 email = "admin@example.com"
-password = "Admin12345"
+password = "testpass123"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
